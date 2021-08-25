900ac8c935841289d510077c2ffd943b5d32e0ee
berheg <ink42310@yahoo.com>
2021-08-25 23:51:14 +0200

    mineblock module added

--- blockchain.js
+++ blockchain.js
@@ -6,14 +6,23 @@ class Block{
     this.data = data;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
   calculateHash(){
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+  }
+  mineBlock(difficulty){
+    while(this.hash.substring(0, difficulty)!== Array(difficulty +1).join("0")){
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log("Block mined: " + this.hash);
   }
 }
 class Blockchain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 2;
   }
   /**
    * @returns {Block}
@@ -33,11 +42,13 @@ class Blockchain {
   }
   addBlock(newBlock){
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
   }
 }
 let ghCoin = new Blockchain();
+console.log("Mining block 1...");
 ghCoin.addBlock(new Block(1, "10/08/2021", {amount: 4}));
+console.log("Minin block 2...");
 ghCoin.addBlock(new Block(1, "11/08/2021", {amount: 10}));
 console.log(JSON.stringify(ghCoin, null, 4));
